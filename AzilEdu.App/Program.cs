90a5b258ed1766dcba4ac0bb951bf93324143b23
Petar Grabic <petarperograbic@gmail.com>
2026-06-10 23:39:49 +0200

    pocetni setup

--- AzilEdu.App/Program.cs
+++ AzilEdu.App/Program.cs
@@ -1,6 +1,9 @@
 using AzilEdu.App.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
